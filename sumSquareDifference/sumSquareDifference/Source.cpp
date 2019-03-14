5c0a4d8df07d73b5b5ba6b99d018b952f0117f9f
Wade Abel <wadejamesabel@gmail.com>
2019-03-14 11:55:48 -0700

    Challenge complete

---
+++ sumSquareDifference/sumSquareDifference/Source.cpp
@@ -0,0 +1,38 @@
+﻿//The sum of the squares of the first ten natural numbers is,  2 + (2)2 + ... + (10)2 = 385
+//The square of the sum of the first ten natural numbers is,    (1 + 2 + ... + 10)2 = (55)2 = 3025
+//Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+//Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+
+#include <iostream>
+#include <cmath>
+
+
+
+
+int main() {
+
+	long long int squared = 0;
+	long long int notSquared = 0;
+
+	//Loop through and get both values
+	for (size_t i = 0; i < 101; i++) {
+		squared += std::pow(i, 2);
+		notSquared += i;
+	}
+
+	//Find the difference and print result
+	notSquared = std::pow(notSquared, 2) - squared;
+	std::cout << "The result is : " << notSquared << std::endl;
+
+
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
